427f5c80ad8e84ecd83d486de491000466e23496
savana-s <savanasathre@gmail.com>
2020-04-17 00:43:05 -0700

    0.0.0
    Created simple hello world landing page.
    With componant layout.

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+export default class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div>
+          <div>
+            <h2>Hello World</h2>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
